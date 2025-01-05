3f6bb1c037295d2e02cf54aab0f45ef55d72e6f9
김나영 <kny200208@naver.com>
2025-01-06 00:50:33 +0900

    Update DemoApplication.java

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -8,12 +8,6 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure()
-				.directory("demo") // 현재 프로젝트 root 기준
-				.load();
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
